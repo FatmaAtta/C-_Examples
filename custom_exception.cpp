1766ea11e8c7bb5352204dc6a59f59258f1f2263
Fatma Atta <fatmamaali@gmail.com>
2023-12-29 01:13:16 +0200

    throw list

--- custom_exception.cpp
+++ custom_exception.cpp
@@ -1,34 +1,42 @@
-//#include <iostream>
-//#include <exception>
-//using namespace std;
-//
-//class Custom_Exception:public exception{
-//public:
-//    virtual const char* what() const noexcept{ //noexcept means this function will not throw an exception
-//        return "Custom";
-//    }
-//};
-////function that throws an exception
-//void throw_exception(){
-//    throw 20;
-//}
-//
-//int main(){
-//    try{
-////      throw 20;
-////      you can create a function that throws an exception
-////        throw "Hello";
+#include <iostream>
+#include <exception>
+using namespace std;
+
+class Custom_Exception:public exception{
+public:
+    virtual const char* what() const noexcept{ //noexcept means this function will not throw an exception
+        return "Custom";
+    }
+};
+//function that throws an exception
+void throw_exception(){
+    throw 20;
+}
+//trying throw list (specifying the types of exceptions that can be thrown)
+void no_throw_list()throw(){
+//    throw "hello";
+}
+//doesnt work in c++17, and above maybe
+void throw_list() /*throw(int,char)*/{
+    throw "hello";
+}
+int main(){
+    try{
+//      throw 20;
+//      you can create a function that throws an exception
+//        throw "Hello";
 //        throw_exception();
-//        throw Custom_Exception();
-//    }
-//    catch(int num){
-//        cout<<"caught int\n"; //since we didnt use the int num while handling the exception, we can just : catch(int)
-//    }
-//    catch(Custom_Exception& c){
-//        cout<<c.what();
-//    }
-//    catch(...){
-//        cout<<"Default catch\n";
-//    }
-//    return 0;
-//}
+        no_throw_list(); //error because the function isn't supposed to throw anything
+        throw Custom_Exception();
+    }
+    catch(int num){
+        cout<<"caught int\n"; //since we didnt use the int num while handling the exception, we can just : catch(int)
+    }
+    catch(Custom_Exception& c){
+        cout<<c.what();
+    }
+    catch(...){
+        cout<<"Default catch\n";
+    }
+    return 0;
+}
